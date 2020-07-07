f8dac661510af80539c8aea0c569cde460e863a9
Dwight Huggins <dwighthuggins@DwightHugginsJr.local>
2020-07-07 15:16:54 -0400

    package-lock.json added

--- server.js
+++ server.js
@@ -5,8 +5,18 @@ const app = express();
 // Add a Port
 const PORT = process.env.PORT || 3000;
 
+// Add Data Processing for POST ROUTES.
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// View / HTML
+app.get("/", (req, res) => {
+    res.send("Hello World!")
+})
+// API / JSON
 
 // Listen on that PORT
 app.listen(PORT, (req, res) => {
     console.log(`Currently running on http://localhost:${PORT}`);
 });
+
